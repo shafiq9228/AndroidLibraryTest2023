b7d5eea4cb9ef28f4c0464c317fcb304359e156b
shafiqpatel <shafiqpatel@gmail.com>
2023-09-05 17:30:12 +0530

    new2

--- app/src/main/java/com/shaf/librarytest2023/MainActivity.kt
+++ app/src/main/java/com/shaf/librarytest2023/MainActivity.kt
@@ -2,10 +2,12 @@ package com.shaf.librarytest2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shaf.mylibrary.MyToaster1
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
